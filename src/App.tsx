e8e8c06e5126d4a13a138a61d8736f68e588aa05
AxelcichelloArciel <acichello@arciel.com>
2025-04-08 16:36:51 -0300

    solucion '0' en el app

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useEffect, useMemo } from "react";
-import { useBudget } from "./Hooks/useBudget";
-import BudgetForm from "./components/BudgeForm";
-import BudgetTracker from "./components/BudgetTracker";
-import ExpenseModal from "./components/ExpenseModal";
-import ExpenseList from "./components/ExpenseList";
-import FilterByCategory from "./components/FilterByCategory";
+import { useEffect, useMemo } from "react"
+import { useBudget } from "./Hooks/useBudget"
+import BudgetTracker from "./components/BudgetTracker"
+import ExpenseModal from "./components/ExpenseModal"
+import ExpenseList from "./components/ExpenseList"
+import FilterByCategory from "./components/FilterByCategory"
+import BudgetForm from "./components/BudgeForm"
 
-function App() {
-  const { state } = useBudget();
-
-  //verificar si el presupuesto es valido
-  const isValidBudget = useMemo(() => state.budget, [state.budget]);
+function App() {  
+  const { state } = useBudget()
+  const isValidBudget = useMemo(() => state.budget > 0, [state.budget])
 
   useEffect(() => {
     localStorage.setItem('budget', state.budget.toString())
     localStorage.setItem('expenses', JSON.stringify(state.expenses))
   }, [state])
-
+  
   return (
     <>
-      <header>
-        <div className="bg-blue-600 py-8 mx-h-72">
-          <h1 className="uppercase text-center font-black text-4xl text-white">
-            Planificador de gastos
-          </h1>
-        </div>
+      <header className="bg-blue-600 py-8 max-h-72">
+        <h1 className="uppercase text-center font-black text-4xl text-white">
+          Planificador de Gastos
+        </h1>
       </header>
 
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-        {isValidBudget ? <BudgetTracker /> : <BudgetForm />}
+          {isValidBudget ? <BudgetTracker />  : <BudgetForm />}
       </div>
 
       {isValidBudget && (
@@ -39,7 +35,7 @@ function App() {
         </main>
       )}
     </>
-  );
+  )
 }
 
-export default App;
+export default App
